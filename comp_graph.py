54764d71d68dbd7debe68313d5ab3bd6363a8380
Carlo <hesse@Macintosh.local>
2024-01-27 15:54:56 +0100

    added diagnostics and quality report from sdv libary

--- comp_graph.py
+++ comp_graph.py
@@ -1,14 +1,32 @@
 from sdv.evaluation.single_table import get_column_plot
 import pandas as pd
 from sdv.metadata import SingleTableMetadata
+from sdv.evaluation.single_table import evaluate_quality
+from sdv.evaluation.single_table import run_diagnostic
 
-real_data = pd.read_csv("results/base_data.csv")
+real_data = pd.read_csv("results/training_data_minority.csv")
 synthetic_data = pd.read_csv("results/synthetic_data.csv")
 #synthetic_data = synthetic_data.drop('Unnamed: 0.1', axis=1)
 
 metadata = SingleTableMetadata()
 metadata.detect_from_csv(filepath="results/base_data.csv")
 
+
+diagnostic = run_diagnostic(
+    real_data=real_data,
+    synthetic_data=synthetic_data,
+    metadata=metadata
+)
+
+quality_report = evaluate_quality(
+    real_data,
+    synthetic_data,
+    metadata
+)
+
+#print(quality_report.get_details('Column Shapes'))
+
+'''
 fig = get_column_plot(
     real_data=real_data,
     synthetic_data=synthetic_data,
@@ -17,3 +35,4 @@ fig = get_column_plot(
 )
 
 fig.show()
+'''
