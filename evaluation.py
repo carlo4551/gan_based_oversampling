faa25ea3c1b8e46abc46ac49671fed06071c190d
Carlo <hesse@Macintosh.local>
2024-01-27 16:09:43 +0100

    moved evaluation from comp_graph.py to evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sdv.metadata import SingleTableMetadata
+from sdv.evaluation.single_table import evaluate_quality
+from sdv.evaluation.single_table import run_diagnostic
+
+real_data = pd.read_csv("results/training_data_minority.csv")
+synthetic_data = pd.read_csv("results/synthetic_data.csv")
+#synthetic_data = synthetic_data.drop('Unnamed: 0.1', axis=1)
+
+metadata = SingleTableMetadata()
+metadata.detect_from_csv(filepath="results/base_data.csv")
+
+diagnostic = run_diagnostic(
+    real_data=real_data,
+    synthetic_data=synthetic_data,
+    metadata=metadata
+)
+
+quality_report = evaluate_quality(
+    real_data,
+    synthetic_data,
+    metadata
+)
